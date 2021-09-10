4f7ab50597f9160ba9ea2aaec9f82626a107408d
Steven Bal <steven@maykinmedia.nl>
2021-09-10 16:04:05 +0200

    :white_check_mark: Add test for extra derived fields on admin form

--- tests/test_admin_form.py
+++ tests/test_admin_form.py
@@ -56,6 +56,49 @@ def test_derive_endpoints_success():
     )
 
 
+@pytest.mark.django_db
+def test_derive_endpoints_extra_field():
+    form_data = {
+        "oidc_rp_client_id": "clientid",
+        "oidc_rp_client_secret": "secret",
+        "oidc_rp_sign_algo": "RS256",
+        "oidc_op_discovery_endpoint": "http://discovery-endpoint.nl/",
+        "claim_mapping": get_claim_mapping(),
+        "groups_claim": "roles",
+    }
+
+    class ExtendedOpenIDConnectConfigForm(OpenIDConnectConfigForm):
+        required_endpoints = OpenIDConnectConfigForm.required_endpoints
+        # Define an extra field to derive from the configuration
+        oidc_mapping = dict(
+            **OpenIDConnectConfigForm.oidc_mapping,
+            **{"logout_endpoint": "end_session_endpoint"}
+        )
+
+    form = ExtendedOpenIDConnectConfigForm(data=form_data)
+
+    configuration = {
+        "authorization_endpoint": "http://provider.com/auth/realms/master/protocol/openid-connect/auth",
+        "token_endpoint": "http://provider.com/auth/realms/master/protocol/openid-connect/token",
+        "userinfo_endpoint": "http://provider.com/auth/realms/master/protocol/openid-connect/userinfo",
+        "jwks_uri": "http://provider.com/auth/realms/master/protocol/openid-connect/certs",
+        "end_session_endpoint": "http://provider.com/auth/realms/master/protocol/openid-connect/logout",
+    }
+    with requests_mock.Mocker() as m:
+        m.get(
+            "http://discovery-endpoint.nl/.well-known/openid-configuration",
+            json=configuration,
+        )
+        assert form.is_valid()
+
+    # The endpoint that was added to the mapping on the extended form
+    # should be present in the cleaned data
+    assert (
+        form.cleaned_data["logout_endpoint"]
+        == "http://provider.com/auth/realms/master/protocol/openid-connect/logout"
+    )
+
+
 @patch("requests.get", side_effect=RequestException)
 def test_derive_endpoints_request_error(*m):
     form_data = {
