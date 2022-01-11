ebb67cbdc4ede69d8e5e81c44626b29fe2dbb092
Steven Bal <steven@maykinmedia.nl>
2022-01-11 09:23:19 +0100

    :bug: Only retrieve singletonmodel when retrieving settings
    fixes: https://github.com/maykinmedia/mozilla-django-oidc-db/issues/30
    
    instead of on backend __init__, otherwise permission checking causes a lot of cache lookups.
    Additionally, cache the configuration on the view, backend and middleware instances

--- mozilla_django_oidc_db/middleware.py
+++ mozilla_django_oidc_db/middleware.py
@@ -7,8 +7,8 @@ from .models import OpenIDConnectConfig
 
 class SessionRefresh(SoloConfigMixin, _SessionRefresh):
     def process_request(self, request):
-        config = OpenIDConnectConfig.get_solo()
-        if not config.enabled:
+        self.refresh_config()
+        if not self.config.enabled:
             return
 
         return super().process_request(request)
