9282a02b42f4c9d5460e59176c8c0aae53b0c2af
Steven Bal <steven@maykinmedia.nl>
2022-01-14 13:59:01 +0100

    :art: Remove unused imports

--- mozilla_django_oidc_db/middleware.py
+++ mozilla_django_oidc_db/middleware.py
@@ -1,8 +1,6 @@
 from mozilla_django_oidc.middleware import SessionRefresh as _SessionRefresh
-from mozilla_django_oidc.utils import import_from_settings
 
 from .mixins import SoloConfigMixin
-from .models import OpenIDConnectConfig
 
 
 class SessionRefresh(SoloConfigMixin, _SessionRefresh):
