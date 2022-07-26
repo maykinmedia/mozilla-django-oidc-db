3a205292df2a443d9db8474ea5772d23073f73be
Steven Bal <steven@maykinmedia.nl>
2022-07-26 17:14:35 +0200

    :goal_net: Catch validation errors during auth process

--- mozilla_django_oidc_db/views.py
+++ mozilla_django_oidc_db/views.py
@@ -1,5 +1,7 @@
+import logging
+
 from django.contrib import admin
-from django.core.exceptions import PermissionDenied
+from django.core.exceptions import PermissionDenied, ValidationError
 from django.db import IntegrityError, transaction
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView
@@ -11,6 +13,7 @@ from mozilla_django_oidc.views import (
 
 from .mixins import SoloConfigMixin
 
+logger = logging.getLogger(__name__)
 OIDC_ERROR_SESSION_KEY = "oidc-error"
 
 
@@ -26,8 +29,16 @@ class OIDCCallbackView(OIDCAuthenticationCallbackView):
             # ensure errors don't lead to half-created users
             with transaction.atomic():
                 response = super().get(request)
-        except IntegrityError as exc:
-            request.session[OIDC_ERROR_SESSION_KEY] = exc.args[0]
+        except (IntegrityError, ValidationError) as exc:
+            logger.exception(
+                "Something went wrong while attempting to authenticate via OIDC"
+            )
+
+            # Remove unicode characters, because these give issues when storing session
+            # data apparently
+            request.session[OIDC_ERROR_SESSION_KEY] = (
+                exc.args[0].encode("ascii", "ignore").decode()
+            )
             return self.login_failure()
         else:
             if OIDC_ERROR_SESSION_KEY in request.session:
