e8b216df86e84707ac76e2bd1ad51eb2b3e168df
Sergei Maertens <sergei@maykinmedia.nl>
2024-02-05 14:29:55 +0100

    :rotating_light: Remove unused import

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,7 +1,6 @@
 from unittest.mock import patch
 
 from django.contrib.auth import get_user_model
-from django.core.exceptions import ValidationError
 from django.db import IntegrityError
 from django.test import TestCase
 from django.urls import reverse
