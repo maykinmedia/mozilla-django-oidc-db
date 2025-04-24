5e0602b960c632ba5e77e8a8bdf8d877b853306a
SilviaAmAm <silvia@maykinmedia.nl>
2025-04-24 14:57:12 +0200

    :recycle: [#97] Refactor backend to use new configuration models

--- mozilla_django_oidc_db/typing.py
+++ mozilla_django_oidc_db/typing.py
@@ -3,6 +3,10 @@ from __future__ import annotations
 from collections.abc import Sequence
 from typing import Protocol, TypeAlias
 
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    AnonymousUser,
+)
 from django.http import HttpRequest, HttpResponseBase
 
 JSONPrimitive: TypeAlias = str | int | float | bool | None
@@ -10,6 +14,8 @@ JSONValue: TypeAlias = "JSONPrimitive | list[JSONValue] | JSONObject"
 JSONObject: TypeAlias = dict[str, JSONValue]
 
 ClaimPath: TypeAlias = Sequence[str]
+
+AnyUser: TypeAlias = AnonymousUser | AbstractBaseUser
 
 
 class DjangoView(Protocol):
